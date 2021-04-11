52d3b600b0e14166e326656a221272d01b881d61
SoNiceElijah <Superil2q@yandex.ru>
2021-04-11 20:50:30 +0300

    script updated

--- scripts/bc.sh
+++ scripts/bc.sh
@@ -2,8 +2,8 @@ SCRIPT_PATH=$(dirname "$0")
 
 cd $SCRIPT_PATH
 cd ..
-if [ -d "$SCRIPT_PATH/build" ]; then
-  rm -rf $SCRIPT_PATH/build
+if [ -d "./build" ]; then
+  rm -rf ./build
 fi
 mkdir build
 cd build
