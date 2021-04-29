3692c7621ed8ece9230933112025215ef50af8d8
polegayko <victorpolegayko@gmail.com>
2021-04-29 18:31:54 +0300

    Changes

--- source/source.cpp
+++ source/source.cpp
@@ -26,6 +26,8 @@ int main()
 	double Rm;
 	double Eps;
 	double taum_p;
+	int n;
+	int k;
 	cout << "Введите R:";
 	cin >> R;
 	cout << "Введите w:";
@@ -35,6 +37,10 @@ int main()
 	cout << "Введите координаты M0:";
 	cin >> x0;
 	cin >> y0;
+	cout << "Введите n:";
+	cout << "Введите k:";
+	cin >> n;
+	cin >> k;
 	vector <double> xf4;
 	vector <double> xf3;
 	vector <double> xf6;
@@ -52,6 +58,7 @@ int main()
 	vector <double> Gammaf6p;
 	int C1;
 	int C2;
+
 	double g2;
 	double bd;
 	double bd1;
@@ -68,90 +75,95 @@ int main()
 	gamma = step;
 
 	do {
-		eta0 = pow((y0*y0 - 2 * (w - 1)*x0), 0.5);
-		tau = 1 / (1 - w)*(y0 + eta0);
-		y1 = R * eta0 + (w - 1)*(gamma - tau);
-		x1 = (R*eta0 + (w - 1)*(gamma - tau)*0.5)*(gamma - tau);
-		if ((x1 >= 0) && (tau < gamma) && (tau >= 0))
+		for (int ix = 1; ix <= n; ix++)
 		{
-			xf4.push_back(x1);
-			yf4.push_back(y1);
-			Gammaf4.push_back(gamma);
-		}
+			eta0 = pow((y0*y0 - 2 * (w - 1)*x0), 0.5);
+			tau = 1 / (1 - w)*(y0 + eta0);
+			y1 = R * eta0 + (w - 1)*(gamma - tau);
+			x1 = (R*eta0 + (w - 1)*(gamma - tau)*0.5)*(gamma - tau);
+			if ((x1 >= 0) && (tau < gamma) && (tau >= 0))
+			{
+				xf4.push_back(x1);
+				yf4.push_back(y1);
+				Gammaf4.push_back(gamma);
+			}
 
-		else
-		{
-			y1 = y0 + (w - 1)*gamma;
-			x1 = x0 + gamma * y0 + (w - 1)*pow(gamma, 2) / 2;
-			m = 0;
-			if (x1 < 0) goto next_cycle;
 			else
 			{
-				xf3.push_back(x1);
-				yf3.push_back(y1);
-				Gammaf3.push_back(gamma);
+				y1 = y0 + (w - 1)*gamma;
+				x1 = x0 + gamma * y0 + (w - 1)*pow(gamma, 2) / 2;
+				m = 0;
+				if (x1 < 0) goto next_cycle;
+				else
+				{
+					xf3.push_back(x1);
+					yf3.push_back(y1);
+					Gammaf3.push_back(gamma);
+				}
 			}
-		}
 
 
-		x2 = x1;
-		y2 = y1;
+			x2 = x1;
+			y2 = y1;
 
-		taum = 0;
-		m = 0;
-		do
-		{
-			taum_p = taum;
-			m++;
-			x1 = x2;
-			y1 = y2;
-			Rm = pow(R, m);
-
-			g2 = 1 / (2 - gamma);
-
-
-			bd = 1 / (gamma * (w - 1) + 2);
-
-
-			bd1 = 1 / (R - 1);
-			eta1 = (y2*y2 + 2 * (gamma*w*bd1 + 1)*x1);
-			eta1 = pow(eta1, 0.5);
-			taum = (2 - gamma)*bd*(y1 + bd1 * eta1*(1 + R - 2 * Rm));
-			y2 = Rm * eta1 - (2 - gamma - taum)*(gamma * w * g2 + 1);
-			x2 = Rm * eta1 - 0.5*(2 - gamma - taum)*(gamma * w*g2 + 1)*(2 - gamma - taum);
-
-			if (m > 1)
+			taum = 0;
+			m = 0;
+			do
 			{
-				xf6.push_back(x1);
-				yf6.push_back(y1);
-				Gammaf6.push_back(gamma);
-			}
-		} while (((x2 >= 0) || ((taum >= 0) && (taum <= 2 - gamma))) && (abs(taum - taum_p) > Eps));
+				taum_p = taum;
+				m++;
+				x1 = x2;
+				y1 = y2;
+				Rm = pow(R, m);
+
+				g2 = 1 / (2 - gamma);
 
 
+				bd = 1 / (gamma * (w - 1) + 2);
 
-		if (m == 1)
-		{
-			x2 = x1 + (2 - gamma)*y1 - 0.5*gamma * (w - 1) + 2 * (2 - gamma);
-			if (x2 >= 0)
-			{
-				y2 = y1 - (w - 1)*gamma - 2;
-				xf5.push_back(x2);
-				yf5.push_back(y2);
-				Gammaf5.push_back(gamma);
-			}
+
+				bd1 = 1 / (R - 1);
+				eta1 = (y2*y2 + 2 * (gamma*w*bd1 + 1)*x1);
+				eta1 = pow(eta1, 0.5);
+				taum = (2 - gamma)*bd*(y1 + bd1 * eta1*(1 + R - 2 * Rm));
+				y2 = Rm * eta1 - (2 - gamma - taum)*(gamma * w * g2 + 1);
+				x2 = Rm * eta1 - 0.5*(2 - gamma - taum)*(gamma * w*g2 + 1)*(2 - gamma - taum);
+				/*
+				if ((m > 1) && (ix > n - k))
+				{
+					xf6.push_back(x1);
+					yf6.push_back(y1);
+					Gammaf6.push_back(gamma);
+				}
+				*/
+			} while (((x2 >= 0) || ((taum >= 0) && (taum <= 2 - gamma))) && (abs(taum - taum_p) > Eps));
+
+
+			if (ix > n - k)
+				if (m == 1)
+				{
+					x2 = x1 + (2 - gamma)*y1 - 0.5*gamma * (w - 1) + 2 * (2 - gamma);
+					if (x2 >= 0)
+					{
+						y2 = y1 - (w - 1)*gamma - 2;
+						xf5.push_back(x2);
+						yf5.push_back(y2);
+						Gammaf5.push_back(gamma);
+					}
+				}
+				else if (abs(taum - taum_p) > Eps)
+				{
+					C1 = y1 - taum_p + 2 * log(taum_p);
+					C2 = x1 - (taum_p*taum_p)*0.5 + 2 * taum_p*log(taum_p);
+					y2 = (2 - gamma) - 2 * log(2 - gamma) + C1;
+					x2 = 0.5*(2 - gamma)*(2 - gamma) + (2 - gamma)*(C1 + 2) - 2 * (2 - gamma)*log(2 - gamma) + C2;
+					xf6p.push_back(x2);
+					yf6p.push_back(y2);
+					Gammaf6p.push_back(gamma);
+				}
+			x0 = x2;
+			y0 = y2;
 		}
-		else if (abs(taum - taum_p) > Eps)
-		{
-			C1 = y1 - taum_p + 2 * log(taum_p);
-			C2 = x1 - (taum_p*taum_p)*0.5 + 2 * taum_p*log(taum_p);
-			y2 = (2 - gamma) - 2 * log(2 - gamma) + C1;
-			x2 = 0.5*(2 - gamma)*(2 - gamma) + (2 - gamma)*(C1 + 2) - 2 * (2 - gamma)*log(2 - gamma) + C2;
-			xf6p.push_back(x2);
-			yf6p.push_back(y2);
-			Gammaf6p.push_back(gamma);
-		}
-
 	next_cycle:
 		gamma = gamma + step;
 	} while (gamma < 2);
@@ -209,4 +221,4 @@ int main()
 
 
 
-}
+} 
